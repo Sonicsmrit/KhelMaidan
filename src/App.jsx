b2859f10af87b5bbf2643ca21631672b790c8059
arunakhadka79-prog <arunakhadka79@gmail.com>
2026-06-11 09:57:29 +0545

    Sign in chanages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import SignIn from './components/SignIn'
-import Login from './components/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Search from './pages/search'
 import Venues from './pages/venues'
+import SignIn from '.pages/signin'
+import Login from '.pages/login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
       </Routes>
